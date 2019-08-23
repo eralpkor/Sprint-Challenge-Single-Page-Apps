1d93d4596ec983ffcd869d59f45b4903934856f6
eralpkor <eralp.ny@gmail.com>
2019-08-23 11:44:39 -0400

    Card UI Loading Data Successfully

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { Card, Icon, Grid, Image } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const { image, name, species, location, origin, episode } = props.person;
+
+  return (
+    <Grid.Column>
+      <Card>
+        <Image src={image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>
+            <span className="date">{species}</span>
+          </Card.Meta>
+          <Card.Description>Location: {location.name}</Card.Description>
+          <Card.Description>Origin: {origin.name}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            {episode.length} Episodes
+          </a>
+        </Card.Content>
+      </Card>
+    </Grid.Column>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import { Grid } from "semantic-ui-react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependencies array!
+    axios.get("https://rickandmortyapi.com/api/character/").then(res => {
+      console.log(res.data.results);
+      setCharacter(res.data.results);
+    });
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Grid container columns={2}>
+        {character.length ? (
+          character.map(person => {
+            return <CharacterCard key={person.id} person={person} />; // return Person component
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Grid>
     </section>
   );
 }
